7f638b76a72118d95632f7d62ee4ccde58be22b0
Lelana <mstlelanakhatun@gmail.com>
2023-05-23 16:43:59 -0300

    ruest commit

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
         }
       }
       const result = await ToysCollection.updateOne(filter, ToyUpdated, option);
-      res.send(result);
+      res.send();
     })
 
     // Delete Single Data
